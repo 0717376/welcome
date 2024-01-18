e68e7f635cbf1e0321bb2cd82c6a2f6432567569
Sergey Muravskiy <0717376@gmail.com>
2024-01-18 15:03:13 +0300

    minor corrections

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -38,6 +38,36 @@ document.addEventListener('DOMContentLoaded', function() {
         localStorage.setItem('checkedDocuments', JSON.stringify(checkedIndexes));
     }
 
+    initMap();
+
+    async function initMap() {
+        // Промис `ymaps3.ready` будет зарезолвлен, когда загрузятся все компоненты основного модуля API
+        await ymaps3.ready;
+    
+        const {YMap, YMapDefaultSchemeLayer} = ymaps3;
+    
+        // Иницилиазируем карту
+        const map = new YMap(
+            // Передаём ссылку на HTMLElement контейнера
+            document.getElementById('map'),
+    
+            // Передаём параметры инициализации карты
+            {
+                location: {
+                    // Координаты центра карты
+                    center: [37.588144, 55.733842],
+    
+                    // Уровень масштабирования
+                    zoom: 10
+                }
+            }
+        );
+    
+        // Добавляем слой для отображения схематической карты
+        map.addChild(new YMapDefaultSchemeLayer());
+    }
+    
+
     // Вопросы FAQ
     const faqItems = document.querySelectorAll('.faq-item h3');
     faqItems.forEach(item => {
@@ -62,3 +92,5 @@ document.addEventListener('DOMContentLoaded', function() {
 // Дополнительные стили для чек-листа и FAQ
 document.styleSheets[0].insertRule('.checked { text-decoration: line-through; }', 0);
 document.styleSheets[0].insertRule('.show { display: block; }', 0);
+
+
