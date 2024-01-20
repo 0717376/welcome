60c0d21a7eb7bb24878ea434450343f689147a45
Sergey Muravskiy <0717376@gmail.com>
2024-01-20 14:27:07 +0300

    minor corrections

--- wwwroot/js/script.js
+++ wwwroot/js/script.js
@@ -85,7 +85,7 @@ async function initMap() {
                 center: [37.588144, 55.733842],
 
                 // Уровень масштабирования
-                zoom: 10
+                zoom: 16
             }
         }
     );
